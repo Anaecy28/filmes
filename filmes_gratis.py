ffafc4e9ec614f7561834658ba84061ef450f406
Anaecy28 <Anaecygaray@gmail.com>
2025-02-01 04:28:57 +0000

    release version 1.0

--- filmes_gratis.py
+++ filmes_gratis.py
@@ -20,9 +20,35 @@ if search_button:
     filtered_df = df_movies[df_movies["name"].str.contains(search_query, case=False, na=False)]
     st.dataframe(filtered_df)
 
+data = {
+    'title': ['Film 1', 'Film 2', 'Film 3', 'Film 4'],
+    'director': ['Director A', 'Director B', 'Director A', 'Director C']
+}
+df = pd.DataFrame(data)
+
+director_selected = st.sidebar.selectbox(
+    "Seleccione un director",
+    options=df['director'].unique(),
+    index=0  
+)
+
+def filter_films_by_director(director):
+    filtered_df = df[df['director'] == director]
+    return filtered_df
+
+if st.sidebar.button("Buscar Filmes"):
+    filtered_films = filter_films_by_director(director_selected)
+    
+    st.write(f"Filmes encontrados de {director_selected}:")
+    st.dataframe(filtered_films) 
+    st.write(f"Total de filmes encontrados: {len(filtered_films)}")
+
+
+
 key_dict = st.secrets["textkey"]
 st.write(key_dict)  
-#key_dict["private_key"] = key_dict["private_key"].replace("\n", "")
+key_dict["private_key"] = key_dict["private_key"].replace("\\n", "\n")
+#creds = service_account.Credentials.from_service_account_info(key_dict)
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project=key_dict["project_id"])
 st.write("Conexión a Firestore exitosa")
