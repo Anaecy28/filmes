860b874868a8c04baf0af9a6ca426810165e33ae
Anaecy28 <Anaecygaray@gmail.com>
2025-02-01 03:45:08 +0000

    release version 1.0

--- filmes_gratis.py
+++ filmes_gratis.py
@@ -5,40 +5,29 @@ from google.oauth2 import service_account
 import json 
 
 key_dict = json.loads(st.secrets["textkey"])
+st.write(key_dict)
+#key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project=key_dict["project_id"])
-st.write("Conexión a Firestore exitosa")
+st.error("Conexión a Firestore exitosa")
 
 def read_from_firestore():
-    movies_ref = db.collection("movies")
-    docs = movies_ref.stream()
+    try:
+        movies_ref = db.collection("movies")
+        docs = movies_ref.stream()
 
-    movies_list = []
-    for doc in docs:
-        movies_list.append(doc.to_dict())
+        movies_list = []
+        for doc in docs:
+            movies_list.append(doc.to_dict())
 
-    return pd.DataFrame(movies_list)
+        if not movies_list:
+            st.write("No se encontraron películas en Firestore.")
+
+        return pd.DataFrame(movies_list)
+
+    except Exception as e:
+        st.write(f"Error al leer desde Firestore: {e}")
 
 df_movies = read_from_firestore()
-print(df_movies.head())
-
-df_movies = read_from_firestore()
-st.sidebar.header("Buscar Películas")
-search_query = st.sidebar.text_input("Ingrese el título de la película")
-search_button = st.sidebar.button("Buscar")
-
-if st.sidebar.checkbox("Mostrar todas las películas"):
-    st.dataframe(df_movies)
-
-if search_button:
-    filtered_df = df_movies[df_movies["name"].str.contains(search_query, case=False, na=False)]
-    st.dataframe(filtered_df)
-def read_from_firestore():
-    movies_ref = db.collection("movies")
-    docs = movies_ref.stream()
-
-    movies_list = []
-    for doc in docs:
-        movies_list.append(doc.to_dict())
-
-    return pd.DataFrame(movies_list)
+if not df_movies.empty:
+    st.write(df_movies.head()) 
