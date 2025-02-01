9626ed2edd3114da45d3b7f0abdbe74719876e15
Anaecy28 <Anaecygaray@gmail.com>
2025-02-01 04:03:51 +0000

    release version 1.0

--- filmes_gratis.py
+++ filmes_gratis.py
@@ -6,19 +6,7 @@ import json
 
 st.write(st.secrets["textkey"])  
 
-key_dict = json.loads(st.secrets["textkey"])
-
-key_string = st.secrets["textkey"]
-if isinstance(key_dict, str):
-    key_dict = json.loads(key_string)
-    creds = service_account.Credentials.from_service_account_info(key_dict)
-    db = firestore.Client(credentials=creds, project=key_dict["project_id"])
-    st.write("Conexión a Firestore exitosa")
-else:
-    st.error("El valor de 'textkey' no es un string.")
-
-# Conectar a Firestore
+key_dict = st.secrets["textkey"]
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project=key_dict["project_id"])
-
 st.write("Conexión a Firestore exitosa")
