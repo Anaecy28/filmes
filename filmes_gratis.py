3e8441671361d2b85d839e2c125310591b9bbcb4
Anaecy28 <Anaecygaray@gmail.com>
2025-02-01 04:11:36 +0000

    release version 1.0

--- filmes_gratis.py
+++ filmes_gratis.py
@@ -4,9 +4,24 @@ from google.cloud import firestore
 from google.oauth2 import service_account
 import json 
 
+#df_movies = read_from_firestore()
+df_movies = None
+st.sidebar.header("Buscar Películas")
+search_query = st.sidebar.text_input("Ingrese el título de la película")
+search_button = st.sidebar.button("Buscar")
+
+if st.sidebar.checkbox("Mostrar todas las películas"):
+    st.dataframe(df_movies)
+
+if search_button:
+    filtered_df = df_movies[df_movies["name"].str.contains(search_query, case=False, na=False)]
+    st.dataframe(filtered_df)
+
+
 st.write(st.secrets["textkey"])  
 
 key_dict = st.secrets["textkey"]
+key_dict["private_key"] = key_dict["private_key"].replace("\n", "")
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project=key_dict["project_id"])
 st.write("Conexión a Firestore exitosa")
