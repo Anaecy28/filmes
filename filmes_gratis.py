f3e43c77588e4f7f9ba799729b37f5e78cfb3f88
Anaecy28 <Anaecygaray@gmail.com>
2025-02-01 03:32:33 +0000

    release version 1.0

--- filmes_gratis.py
+++ filmes_gratis.py
@@ -4,56 +4,36 @@ from google.cloud import firestore
 from google.oauth2 import service_account
 import json 
 
-db = firestore.Client.from_service_account_json("firebase.json")
-dbNames = db.collection("names")
+def read_from_firestore():
+    movies_ref = db.collection("movies")
+    docs = movies_ref.stream()
 
-def loadByName(name):
-  names_ref = dbNames.where(u'name', u'==',name)
-  currentName =  None
-  for myname in names_ref.stream():
-    currentName =  myname
-  return currentName
+    movies_list = []
+    for doc in docs:
+        movies_list.append(doc.to_dict())
 
-#BTN BUSCAR
-st.sidebar.subheader("Buscar nombre")
-nameSearch= st.sidebar.text_input("nombre")
-btnFiltrar= st.sidebar.button("Buscar")
+    return pd.DataFrame(movies_list)
 
-if btnFiltrar:
-  doc = loadByName(nameSearch)
-  if doc is None:
-    st.sidebar.write("Nombre no existe")
-  else:
-    st.sidebar.write(doc.to_dict())
+df_movies = read_from_firestore()
+print(df_movies.head())
 
-# BTN ELIMINAR
+df_movies = read_from_firestore()
+st.sidebar.header("Buscar Películas")
+search_query = st.sidebar.text_input("Ingrese el título de la película")
+search_button = st.sidebar.button("Buscar")
 
-st.sidebar.subheader("Eliminar registro")
-nameSearch= st.sidebar.text_input("nombre a eliminar")
-btnEliminar= st.sidebar.button("Eliminar")
+if st.sidebar.checkbox("Mostrar todas las películas"):
+    st.dataframe(df_movies)
 
-if btnEliminar:
-  deleteName = loadByName(nameSearch)
-  if deleteName is None:
-    st.sidebar.write(f"{nameSearch} no existe")
-  else:
-    dbNames.document(deleteName.id).delete()
-    st.sidebar.write(f"{nameSearch} eliminado")
+if search_button:
+    filtered_df = df_movies[df_movies["name"].str.contains(search_query, case=False, na=False)]
+    st.dataframe(filtered_df)
+def read_from_firestore():
+    movies_ref = db.collection("movies")
+    docs = movies_ref.stream()
 
-# BTN ACTUALIZAR
+    movies_list = []
+    for doc in docs:
+        movies_list.append(doc.to_dict())
 
-st.sidebar.subheader("Actualizar registro")
-nameNew= st.sidebar.text_input("nombre a actualizar")
-btnActualizar= st.sidebar.button("Actualizar")
-
-if btnActualizar:
-  actualizarName = loadByName(nameSearch)
-  if actualizarName is None:
-    st.sidebar.write(f"{nameSearch} no existe")
-  else:
-    miActualizarName = dbNames.document(actualizarName.id)
-    miActualizarName.update(
-        {
-            "name": nameNew
-        }
-    )
+    return pd.DataFrame(movies_list)
